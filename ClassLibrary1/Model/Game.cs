e2f676792cd79c291c7bbea98285f1d8582e4b0f
Matheus Delgado <matheus.delgado05@gmail.com>
2025-12-10 17:07:36 +0000

    Finished MakeMove, now it's possible to castle and en passant. Added  an avoid selfcheck logic.

--- ClassLibrary1/Model/Game.cs
+++ ClassLibrary1/Model/Game.cs
@@ -3,8 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using 
+using System.Threading.Tasks; 
 
 namespace ChessApp.Model.Model
 {
@@ -20,6 +19,7 @@ namespace ChessApp.Model.Model
         public Game()
         {
             Board = new Board();
+            Board.InitializeBoard();
         }
 
         public void MakeMove(Position from, Position to)
@@ -35,10 +35,91 @@ namespace ChessApp.Model.Model
             if (piece.Color != CurrentTurn)
                 throw new InvalidOperationException("Not this piece's turn.");
 
-            if (!piece.CanMoveTo(to, Board))
+
+            if (!piece.CanMoveTo(to, Board)) //logica para o movimento normal
                 throw new InvalidOperationException("Invalid move.");
 
+            if (piece is King king && Math.Abs(to.Column - from.Column) == 2)//logica para o castling
+            {
+                if (to.Column > from.Column)
+                {
+                    if (IsCheck(CurrentTurn))
+                        throw new InvalidOperationException("Cannot castle while in check.");
+
+                    Position middle = new Position(from.Row, (from.Column + to.Column) / 2);
+                    if (IsSquareAttacked(middle, CurrentTurn == Color.White ? Color.Black : Color.White))
+                        throw new InvalidOperationException("Cannot castle through check.");
+
+                    if (IsSquareAttacked(to, CurrentTurn == Color.White ? Color.Black : Color.White))
+                        throw new InvalidOperationException("Cannot castle into check.");
+
+                    if (!king.CanCastleShort(Board))
+                        throw new InvalidOperationException("Cannot castle short.");
+
+                    Piece rook = Board.GetPiece(new Position(from.Row, 7));
+                    Board.MovePiece(new Position(from.Row, 7), new Position(from.Row, 5));
+                }
+                else
+                {
+                    if (!king.CanCastleLong(Board))
+                        throw new InvalidOperationException("Cannot castle long.");
+
+                    Piece rook = Board.GetPiece(new Position(from.Row, 0));
+                    Board.MovePiece(new Position(from.Row, 0), new Position(from.Row, 3));
+                }
+
+                Board.MovePiece(from, to);
+                MoveHistory.Add(new Movement(piece, from, to, null, MovementType.Castling, to.ToChessNotation()));
+                SwitchTurn();
+                return;
+            }
+
+            if (piece is Pawn && from.Column != to.Column && Board.GetPiece(to) == null)
+            {
+                Movement lastMove = MoveHistory.LastOrDefault();
+                if (lastMove != null &&
+                    lastMove.PieceMoved is Pawn &&
+                    Math.Abs(lastMove.From.Row - lastMove.To.Row) == 2 &&
+                    lastMove.To.Column == to.Column &&
+                    lastMove.To.Row == from.Row)
+                {
+                    Position enemyPawnPos = lastMove.To;
+                    Piece capturedPawn = Board.RemovePiece(enemyPawnPos);
+
+                    Board.MovePiece(from, to);
+
+                    if (IsCheck(CurrentTurn))
+                    {
+                        Board.MovePiece(to, from);
+                        Board.PlacePiece(capturedPawn, enemyPawnPos);
+                        throw new InvalidOperationException("Move results in self-check.");
+                    }
+
+                    if (CurrentTurn == Color.White)
+                        WhiteCapturedPieces.Add(capturedPawn);
+                    else
+                        BlackCapturedPieces.Add(capturedPawn);
+
+                    MoveHistory.Add(new Movement(piece, from, to, capturedPawn, MovementType.EnPassant, to.ToChessNotation()));
+                    SwitchTurn();
+                    return;
+                }
+            }
+            bool wasMoved = piece.HasMoved;
             Piece capturedPiece = Board.MovePiece(from, to);
+
+            if (IsCheck(CurrentTurn))
+            {
+                // Reverte o Movimento
+                Board.MovePiece(to, from);
+                piece.HasMoved = wasMoved;
+                if (capturedPiece != null)
+                {
+                    Board.PlacePiece(capturedPiece, to);
+                }
+                throw new InvalidOperationException("Move results in self-check.");
+            }
+
 
             if (capturedPiece != null)
             {
@@ -52,7 +133,7 @@ namespace ChessApp.Model.Model
             PieceType? promotion = null;
 
 
-            if (piece is Pawn pawn && pawn.CanPromote()) //fazer depois a opcao de escolher a peca
+            if (piece is Pawn pawn && pawn.CanPromote()) //fazer depois a opcao de escolher a peca!!!
             {
                 type = MovementType.Promotion;
                 promotion = PieceType.Queen;
@@ -68,6 +149,24 @@ namespace ChessApp.Model.Model
             MoveHistory.Add(move);
 
             SwitchTurn();
+        }
+
+        public bool IsCheck(Color kingColor)
+        {
+            Position kingPos = Board.GetKingPosition(kingColor);
+            Color opponentColor = kingColor == Color.White ? Color.Black : Color.White;
+            return IsSquareAttacked(kingPos, opponentColor);
+        }
+
+        private bool IsSquareAttacked(Position pos, Color attackerColor)
+        {
+            List<Piece> attackers = Board.GetAllPiecesOfColor(attackerColor);
+            foreach (var attacker in attackers)
+            {
+                if (attacker.CanMoveTo(pos, Board))
+                    return true;
+            }
+            return false;
         }
 
         private void SwitchTurn()
