8c99c85c7f071ea457bef1e0580c91d99c2ac5be
Matheus Delgado <matheus.delgado05@gmail.com>
2025-12-17 03:54:17 +0000

    Implemented GameViewModel, SquareVewModel and the MainWindow

---
+++ ChessApp.WPF/ViewModel/GameViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel; //para ObservableCollection que avisa o WPF se adicionar ou remover quadrados
+using ChessApp.Model.Model;
+using System.Windows;
+
+namespace ChessApp.WPF.ViewModel
+{
+    public class GameViewModel : BaseViewModel
+    {
+        public Game Game { get; private set; }
+        public ObservableCollection<SquareViewModel> BoardSquares { get; set; }
+
+        public GameViewModel()
+        {
+            Game = new Game();
+
+            BoardSquares = new ObservableCollection<SquareViewModel>();
+
+            InitializeBoardVisuals();
+
+            RefreshBoard();
+        }
+
+        private void InitializeBoardVisuals()
+        {
+            for(int row = 0; row < 8; row++)
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    var square = new SquareViewModel(new Position(row, col));
+
+                    BoardSquares.Add(square);
+                }
+            }
+        }
+
+        public void RefreshBoard()
+        {
+            foreach(var square in BoardSquares)
+            {
+                Piece piece = Game.Board.GetPiece(square.Position);
+
+                square.UpdatePiece(piece);
+            }
+        }
+    }
+}
---
+++ ChessApp.WPF/ViewModel/SquareViewModel.cs
@@ -0,0 +1,99 @@
+﻿using ChessApp.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ChessApp.Model.Enums;
+using Color = ChessApp.Model.Enums.Color;
+
+namespace ChessApp.WPF.ViewModel
+{
+    public class SquareViewModel : BaseViewModel
+    {
+        public Position Position { get; private set; }
+        public Brush BackgroundColor { get; set; }
+
+        public SquareViewModel(Position position)
+        {
+            Position = position;
+            int soma = position.Row + position.Column;
+
+            if (soma % 2 == 0)
+                BackgroundColor = Brushes.Beige;
+            else
+                BackgroundColor = Brushes.Brown;
+        }
+
+        private string _pieceImage;
+        public string PieceImage
+        {
+            get { return _pieceImage; }
+            set
+            {
+                _pieceImage = value;
+                OnPropertyChanged(); //avisa a tela para atualizar
+            }
+        }
+
+        public void UpdatePiece(Piece piece)
+        {
+            if (piece == null)
+            {
+                PieceImage = null;
+                return;
+            }
+            if(piece.Color == Color.White)
+            {
+                switch (piece.PieceType)
+                {
+                    case PieceType.Pawn:
+                        PieceImage = "/Assets/wP.png";
+                        break;
+                    case PieceType.Rook:
+                        PieceImage = "/Assets/wR.png";
+                        break;
+                    case PieceType.Knight:
+                        PieceImage = "/Assets/wN.png";
+                        break;
+                    case PieceType.Bishop:
+                        PieceImage = "/Assets/wB.png";
+                        break;
+                    case PieceType.Queen:
+                        PieceImage = "/Assets/wQ.png";
+                        break;
+                    case PieceType.King:
+                        PieceImage = "/Assets/wK.png";
+                        break;
+                }
+            }
+            else if(piece.Color == Color.Black)
+            {
+                switch (piece.PieceType)
+                {
+                    case PieceType.Pawn:
+                        PieceImage = "/Assets/bP.png";
+                        break;
+                    case PieceType.Rook:
+                        PieceImage = "/Assets/bR.png";
+                        break;
+                    case PieceType.Knight:
+                        PieceImage = "/Assets/bN.png";
+                        break;
+                    case PieceType.Bishop:
+                        PieceImage = "\\Assets\\bB.png";
+                        break;
+                    case PieceType.Queen:
+                        PieceImage = "/Assets/bQ.png";
+                        break;
+                    case PieceType.King:
+                        PieceImage = "/Assets/bK.png";
+                        break;
+                }
+            }
+        }
+    }
+}
